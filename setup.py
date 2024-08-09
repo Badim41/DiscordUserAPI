c1d39011850bb128d3f8a6c86fbfa41ad6ead4d1
badim41 <ramdomakk@gmail.com>
2024-08-09 16:09:04 +1200

    create repo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DiscordUserAPI',
-    version='0.1',
+    version='0.101',
     packages=find_packages(),
     install_requires=[
         'requests==2.31.0',
