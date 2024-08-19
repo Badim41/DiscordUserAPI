9c983c61d47c1d09d10a70bbfe46739dfcb1e813
badim41 <ramdomakk@gmail.com>
2024-08-20 11:15:46 +1200

    create repo

--- discord_user/global_logger/logs.py
+++ discord_user/global_logger/logs.py
@@ -4,7 +4,6 @@ import sys
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-console_handler.stream = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
 
 logging.basicConfig(level=logging.DEBUG, handlers=[console_handler])
 
