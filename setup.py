7431f0f6620ae87f2c93f4d8977caf640bc6e076
badim41 <ramdomakk@gmail.com>
2024-08-23 00:26:12 +1200

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DiscordUserAPI',
-    version='0.103',
+    version='0.104',
     packages=find_packages(),
     install_requires=[
         'requests==2.31.0',
