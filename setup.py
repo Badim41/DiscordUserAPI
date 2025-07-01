d63748dd03ce9acb974840f256a743a0b48c5652
badim41 <ramdomakk@gmail.com>
2025-07-02 10:08:23 +1200

    исправлены зависимости

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='DiscordUserAPI',
-    version='0.15',
+    version='0.151',
     packages=find_packages(),
     install_requires=[
         'requests~=2.32.0',
         'websockets',
         'aiohttp-socks',
+        'aiohttp==3.8.6',
         'pydub',
         'aiofiles',
-        'httpx'
+        'httpx==0.25.1'
     ],
 )
